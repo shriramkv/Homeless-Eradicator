f03408bd6dca7a7a441323276a413236d90e62f4
RaguRamanTB <aakash1956ragu@gmail.com>
2020-01-22 18:37:35 +0530

    Attachment of Image done in Homeless Identified Activity

--- app/src/main/java/io/RaguRamanTB/homelesseradicator/activities/HomelessIdentifiedActivity.java
+++ app/src/main/java/io/RaguRamanTB/homelesseradicator/activities/HomelessIdentifiedActivity.java
@@ -1,14 +1,32 @@
 package io.RaguRamanTB.homelesseradicator.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import io.RaguRamanTB.homelesseradicator.R;
 
-public class HomelessIdentifiedActivity extends AppCompatActivity {
+public class HomelessIdentifiedActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int PERMISSION_REQUEST = 0;
+    private static final int RESULT_LOAD_IMAGE = 1;
+    private ImageView imageView;
+    private Button attach, pickup, drop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +42,72 @@ public class HomelessIdentifiedActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
+        }
+
+        imageView = findViewById(R.id.attachedImage);
+        attach = findViewById(R.id.attachImage);
+        pickup = findViewById(R.id.pickup);
+        drop = findViewById(R.id.drop);
+
+        attach.setOnClickListener(this);
+        pickup.setOnClickListener(this);
+        drop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this,"Permission Granted!",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denied!",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RESULT_LOAD_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath = cursor.getString(columnIndex);
+                    cursor.close();
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.attachImage:
+                attachImage();
+                break;
+
+            case R.id.pickup:
+                Toast.makeText(this,"Pickup to be done!",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.drop:
+                Toast.makeText(this,"Drop to be done!",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private void attachImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, RESULT_LOAD_IMAGE);
     }
 }
